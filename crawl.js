b132354f5772bda938e12f8b851e5599f4eabdf7
Sunny Purewal <sunnypurewal@gmail.com>
2019-10-10 13:46:41 -0400

    Skipping cache ons itemapper

--- crawl.js
+++ crawl.js
@@ -5,7 +5,7 @@ const fs = require("fs")
 
 const crawl = (domain, since) => {
   return new Promise((resolve, reject) => {
-    const mapper = new getsitemap.SiteMapper()
+    const mapper = new getsitemap.SiteMapper(false)
     mapper.map(domain, since).then((sitemapstream) => {
       resolve(sitemapstream)
     }).catch((err) => {
