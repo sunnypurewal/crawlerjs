88bbaf1dd488fc8c79228eaabdd0fe2698fdc05f
Sunny Purewal <sunnypurewal@gmail.com>
2019-10-15 09:07:46 -0400

    Fixing up crawlerjs to use crawlbot

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 'use strict'
 
-const Server = require("crawlbot-server")
+const crawlbot = require("crawlbot")
+const fs = require("fs")
+const hittp = require("hittp")
 
-const server = new Server()
+let forks = []
+const SINCE = "2019-10-14"
 
-server.onHTML = (html, url) => {
+const main = async () => {
+  fs.readFile("./data/domains.json", (err, data) => {
+    if (err) {
+      console.error(err.message)
+      return
+    }
+
+    let domains = []
+    try {
+      domains = JSON.parse(data.toString())
+    } catch (err) {
+      console.error(err.message)
+      return
+    }
+
+    for (const domain of domains) {
+      while (forks.length >= 10) {
+        await sleep(5000)
+      }
+      const url = hittp.str2url(domain)
+      if (!url) continue
+      const file = fs.createWriteStream(`./data/articles/${url.host}.ndjson`)
+      crawlbot.crawl(url, SINCE, (html, url) => {
+        onHTML(html, url, file)
+      }, () => {
+
+      })
+    }
+  })
 }
 
-server.listen((options) => {
-  console.log(`Crawlbot Server started at ${options.host}:${options.port}`)
-})
+const onHTML = (html, url, writestream) => {
+
+}
+
+main()
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
